15437f650a9bf5b9922fd845900edbd72e2ae42d
ikiskin <ivan.kiskin@wadh.ox.ac.uk>
2021-06-20 00:09:23 +0100

    v1 dash predict neurips BNN

--- Code/lib/predict.py
+++ Code/lib/predict.py
@@ -1,5 +1,6 @@
 import os
 import util
+import util_dashboard as util_dash
 import librosa
 import numpy as np
 import sys
@@ -8,7 +9,7 @@ import argparse
 import matplotlib.pyplot as plt
 
 def write_output(rootFolderPath, audio_format,  dir_out=None, det_threshold=0.5, n_samples=10, feat_type='log-mel',n_feat=128, win_size=30, step_size=30,
-                 n_hop=512, sr=8000, norm_per_sample=True, debug=False, to_filter=False, plot=None):
+                 n_hop=512, sr=8000, norm_per_sample=True, debug=False, to_dash=False):
 
         '''dir_out = None if we want to save files in the same folder that we read from.
            det_threshold=0.5 determines the threshold above which an event is classified as positive. See detect_timestamps for 
@@ -25,10 +26,7 @@ def write_output(rootFolderPath, audio_format,  dir_out=None, det_threshold=0.5,
         mozz_audio_list = []
         
         print('Processing:', rootFolderPath, 'for audio format:', audio_format)
-        print(type(audio_format))
-        print(type('.wav'))
-        if audio_format == '.wav':
-            print('yes')
+
         i_signal = 0
         for root, dirs, files in os.walk(rootFolderPath):
             for filename in files:	
@@ -51,26 +49,20 @@ def write_output(rootFolderPath, audio_format,  dir_out=None, det_threshold=0.5,
                             out.append(model.predict(X_CNN))
 
                         G_X, U_X, _ = util.active_BALD(np.log(out), X_CNN, 2)
-                        preds_list = util.detect_timestamps_BNN(np.repeat(np.mean(out, axis=0), step_size, axis=0),
-                                              np.repeat(G_X, step_size, axis=0),
-                                              np.repeat(U_X, step_size, axis=0), det_threshold=det_threshold)   
 
-                        if to_filter:
-                            preds_filt = np.zeros([len(preds_CNN),2])
-                            preds_filt[:,1] = medfilt(preds_CNN[:,1], kernel_size=51)
-                            preds_filt[:,0] = 1 - preds_filt[:,1]
-                            preds_CNN = preds_filt
+
+                        y_to_timestamp = np.repeat(np.mean(out, axis=0), step_size, axis=0)
+                        G_X_to_timestamp = np.repeat(G_X, step_size, axis=0)
+                        U_X_to_timestamp = np.repeat(U_X, step_size, axis=0)
+                        preds_list = util.detect_timestamps_BNN(y_to_timestamp, G_X_to_timestamp, U_X_to_timestamp, 
+                                                                det_threshold=det_threshold)   
 
                         if debug:
                             print(preds_list)
                             for times in preds_list:
                                 mozz_audio_list.append(librosa.load(os.path.join(root, filename), offset=float(times[0]),
                                                                      duration=float(times[1])-float(times[0]), sr=sr)[0])
-        #                     plt.plot((1/sr)*librosa.frames_to_samples(np.arange(len(np.repeat(np.mean(out, axis=0),step_size, axis=0))), hop_length=512),
-        #                                                    np.repeat(np.mean(out, axis=0), step_size, axis=0)[:,1])
-        #                     plt.xlabel('Seconds')
-        #                     plt.ylabel('Mean probability (mozz)')
-        #                     plt.show()
+
 
                         if dir_out:
                             root_out = root.replace(rootFolderPath, dir_out)
@@ -80,18 +72,21 @@ def write_output(rootFolderPath, audio_format,  dir_out=None, det_threshold=0.5,
   
 
                         if not os.path.exists(root_out): os.makedirs(root_out)
-                        np.savetxt(os.path.join(root_out, filename) + '_BNN_step_' + str(step_size) + '_samples_' + str(n_samples) + '_'
+
+                        if filename.endswith(audio_format):  
+                            output_filename = filename[:-4]  # remove file extension for renaming to other formats.
+                        else:
+                            output_filename = filename # no file extension present
+
+
+                        np.savetxt(os.path.join(root_out, output_filename) + '_BNN_step_' + str(step_size) + '_samples_' + str(n_samples) + '_'
                                 + str(model_name) + '.txt', preds_list, fmt='%s', delimiter='\t')
 
-                        if plot:
-                            plt.figure(figsize=(10,5))
-                            plt.plot((1/sr)*librosa.frames_to_samples(np.arange(len(np.repeat(np.mean(out, axis=0),step_size, axis=0))), hop_length=n_hop),
-                                                            np.repeat(np.mean(out, axis=0), step_size, axis=0)[:,1])
-                            plt.xlabel('Seconds')
-                            plt.ylabel('Mean probability (mozz)')
-                            plt.tight_layout()
-                            plt.savefig(os.path.join(root_out, filename) + '.svg')
-                            plt.close(plt.gcf()) # May be better to re-write to not use plt API
+                        if to_dashboard: 
+                            mozz_audio_filename, audio_length, has_mosquito = util_dash.write_audio_for_plot(text_output_filename, root, filename, output_filename, root_out, sr)
+                            if has_mosquito:
+                                plot_filename = util_dash.plot_mozz_MI(X_CNN, y_to_timestamp[:,1], U_X_to_timestamp, 0.5, root_out, output_filename)
+                                util_dash.write_video_for_dash(plot_filename, mozz_audio_filename, audio_length, root_out, output_filename)
 
 
         print('Total files of ' + str(audio_format) + ' format processed:', i_signal)
@@ -103,7 +98,7 @@ if __name__ == "__main__":
     parser.add_argument("rootFolderPath", help="Source destination of audio files. Can be a parent directory.")
     parser.add_argument("audio_format", help="Any file format supported by librosa load.")
     parser.add_argument("--dir_out", help="Output directory. If not specified, predictions are output to the same folder as source.")
-    parser.add_argument("--plot", help="Save figure of predictions to same directory as dictated by dir_out.")
+    parser.add_argument("--to_dash", help="Save predicted audio, video, and corresponding labels to same directory as dictated by dir_out.")
     parser.add_argument("--norm", default=True, help="Normalise feature windows with respect to themsleves.")
     parser.add_argument("--win_size", default=30, type=int, help="Window size.")
     parser.add_argument("--step_size", default=30, type=int, help="Step size.")
@@ -117,10 +112,10 @@ if __name__ == "__main__":
     rootFolderPath = args.rootFolderPath
     audio_format = args.audio_format
     dir_out = args.dir_out
-    plot = args.plot
+    to_dash = args.to_dash
     win_size = args.win_size
     step_size = args.step_size
     norm_per_sample=args.norm
 
 
-    write_output(rootFolderPath, audio_format, dir_out=dir_out, norm_per_sample=norm_per_sample, win_size=win_size, step_size=step_size, plot=plot)
+    write_output(rootFolderPath, audio_format, dir_out=dir_out, norm_per_sample=norm_per_sample, win_size=win_size, step_size=step_size, to_dash=to_dash)
